663dc46ffaf4ea96cfd631dc61d7958563ac7b1f
Christian Koepp <christian.koepp@tum.de>
2013-06-26 13:24:17 +0200

    version 0.51

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='TwitterSearch',
-      version='0.50',
+      version='0.51',
       description='A library to easily iterate tweets found by the Twitter Search API',
       long_description=readme(),
       url='http://github.com/ckoepp/TwitterSearch',
