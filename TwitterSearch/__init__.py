495d1735614d8726f1f2e4d2d7ab3938ae55594c
Christian Koepp <christian.koepp@tum.de>
2013-08-12 11:28:05 +0200

    Update __init__.py

--- TwitterSearch/__init__.py
+++ TwitterSearch/__init__.py
@@ -5,6 +5,8 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2013 Christian Koepp'
 
 from .TwitterSearch import TwitterSearch
+from .TwitterOrder import TwitterOrder
 from .TwitterSearchOrder import TwitterSearchOrder
+from .TwitterUserOrder import TwitterUserOrder
 from .TwitterSearchException import TwitterSearchException
 from .utils import py3k
